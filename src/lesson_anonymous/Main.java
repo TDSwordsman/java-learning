e8fdfcf6345d71b63f22e09f03c444971cde8c69
TDSwordsman <Arian.Amiri123@mail.ru>
2026-09-11 21:18:38 +0300

    Update Main.java

--- src/lesson_anonymous/Main.java
+++ src/lesson_anonymous/Main.java
@@ -1,4 +1,15 @@
 package lesson_anonymous;
 
 public class Main {
+    public static void main(String[] args) {
+
+        AbleToEat creature = new AbleToEat() {
+            @Override
+            public void eat() {
+                System.out.println("Кто-то ест через анонимный класс!");
+            }
+        };
+
+        creature.eat();
+    }
 }
